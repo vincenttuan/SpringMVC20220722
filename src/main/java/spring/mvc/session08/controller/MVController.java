f229f8b55eb4c78c94d2ffa220ab6ea9d4ca3508
teacher <teacher@BE-403->
2022-07-25 20:59:03 +0800

    s

--- src/main/java/spring/mvc/session08/controller/MVController.java
+++ src/main/java/spring/mvc/session08/controller/MVController.java
@@ -1,16 +1,20 @@
 package spring.mvc.session08.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/mv")
 @Controller
 public class MVController {
 	
+	@GetMapping("/case1")
+	//@RequestMapping(method = {RequestMethod.GET})
 	public ModelAndView case1() {
-		String data = "Hello Model and View";
-		String view = "/WEB-INF/view/show_data.jsp";
+		String data = "Hello Model and View";  // 資料(Model)
+		String view = "/WEB-INF/view/show_data.jsp"; // 渲染(View)
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("data", data);
 		mv.setViewName(view);
