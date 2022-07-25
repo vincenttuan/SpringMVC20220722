31af2c0033ee8a1289b123eacc815218723625af
teacher <teacher@BE-403->
2022-07-25 20:55:49 +0800

    s

---
+++ src/main/java/spring/mvc/session08/controller/MVController.java
@@ -0,0 +1,16 @@
+package spring.mvc.session08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/mv")
+@Controller
+public class MVController {
+	
+	public ModelAndView case1() {
+		String data = "Hello Model and View";
+		String view = ""
+	}
+	
+}
