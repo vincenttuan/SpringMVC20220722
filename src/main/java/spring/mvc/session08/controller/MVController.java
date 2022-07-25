aee0f81893288896bef21763d7b657bd66fdfdf5
teacher <teacher@BE-403->
2022-07-25 21:00:14 +0800

    s

--- src/main/java/spring/mvc/session08/controller/MVController.java
+++ src/main/java/spring/mvc/session08/controller/MVController.java
@@ -3,7 +3,6 @@ package spring.mvc.session08.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/mv")
