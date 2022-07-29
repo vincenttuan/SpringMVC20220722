9cc66e0643512b5c73f77a8158de4a2b58087d2f
teacher <teacher@BE-403->
2022-07-29 19:02:19 +0800

    s

--- src/main/java/spring/mvc/session09/controller/LottoController.java
+++ src/main/java/spring/mvc/session09/controller/LottoController.java
@@ -1,5 +1,28 @@
 package spring.mvc.session09.controller;
 
-public class LottoController {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/lotto")
+public class LottoController {
+	private List<Set<Integer>> lottos = new CopyOnWriteArrayList<>();
+	
+	// Lotto 主畫面
+	//@GetMapping("/get")
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public String index() {
+		return "session09/lotto";
+	}
+	
+	
+	
 }
