78a8f89dbdfebaf8c6f392be48d1e4ae57aa37cf
teacher <teacher@BE-403->
2022-07-29 19:18:03 +0800

    s

--- src/main/java/spring/mvc/session09/controller/LottoController.java
+++ src/main/java/spring/mvc/session09/controller/LottoController.java
@@ -1,12 +1,15 @@
 package spring.mvc.session09.controller;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +26,28 @@ public class LottoController {
 		return "session09/lotto";
 	}
 	
+	// 取得最新電腦選號
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public String get(Model model) {
+		// 取得樂透號碼
+		Set<Integer> lotto = getRandomLotto();
+		// 將最新樂透號碼放在歷史紀錄中
+		lottos.add(lotto);
+		// 將必要資料傳給 jsp 呈現/處理
+		model.addAttribute("lotto", lotto); // 最新電腦選號
+		model.addAttribute("lottos", lottos); // 樂透號碼歷史紀錄
+		return "session09/lotto";
+	}
 	
+	// 隨機生成最新電腦選號
+	private Set<Integer> getRandomLotto() {
+		Random r = new Random();
+		// 樂透 539, 1~39不重複數字取5個
+		Set<Integer> lotto = new LinkedHashSet<>();
+		while(lotto.size() < 5) {
+			lotto.add(r.nextInt(39) + 1);
+		}
+		return lotto;
+	}
 	
 }
