62942f07a8f256e6e276c5b4fb9f99e16b272974
teacher <teacher@BE-403->
2022-08-08 21:21:11 +0800

    s

--- src/main/java/spring/mvc/session12/validator/MyStockValidator.java
+++ src/main/java/spring/mvc/session12/validator/MyStockValidator.java
@@ -2,20 +2,30 @@ package spring.mvc.session12.validator;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+
+import spring.mvc.session12.entity.MyStock;
 
 @Component
 public class MyStockValidator implements Validator {
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
-		return false;
+		// 先判斷傳進來的 clazz 是否是 MyClass 類
+		return MyStock.class.isAssignableFrom(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		// 基礎驗證
+		ValidationUtils.rejectIfEmpty(errors, "symbol", "mystock.symbol.notEmpty");
+		ValidationUtils.rejectIfEmpty(errors, "price", "mystock.price.notEmpty");
+		ValidationUtils.rejectIfEmpty(errors, "amount", "mystock.amount.notEmpty");
+		
+		// 進階驗證
+		
+		
 		
 	}
 
