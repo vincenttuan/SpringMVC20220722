2f1948e6729bd1f6d57bea1a83bf47301d4fc931
teacher <teacher@BE-403->
2022-08-08 21:36:02 +0800

    s

--- src/main/java/spring/mvc/session12/validator/MyStockValidator.java
+++ src/main/java/spring/mvc/session12/validator/MyStockValidator.java
@@ -6,6 +6,8 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import spring.mvc.session12.entity.MyStock;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 @Component
 public class MyStockValidator implements Validator {
@@ -23,7 +25,18 @@ public class MyStockValidator implements Validator {
 		ValidationUtils.rejectIfEmpty(errors, "price", "mystock.price.notEmpty");
 		ValidationUtils.rejectIfEmpty(errors, "amount", "mystock.amount.notEmpty");
 		
+		MyStock myStock = (MyStock)target;
 		// 進階驗證
+		try {
+			Stock stock = YahooFinance.get(myStock.getSymbol());
+			if(stock == null) {
+				errors.rejectValue("symbol", "mystock.symbol.notFound");
+			} else {
+				
+			}
+		} catch (Exception e) {
+			errors.rejectValue("yahoo", "yahoo.finance.error");
+		}
 		
 		
 		
