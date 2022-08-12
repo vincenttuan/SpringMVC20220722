a89985eacab548d6951e66b2e117a4e551475f84
teacher <teacher@BE-403->
2022-08-12 19:21:24 +0800

    s

--- src/main/java/spring/mvc/session13/controller/NowController.java
+++ src/main/java/spring/mvc/session13/controller/NowController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.mvc.session13.repository.NowDao;
 
+//@Controller
 @RestController
 @RequestMapping("/jdbc/now")
 public class NowController {
@@ -16,6 +18,7 @@ public class NowController {
 	private NowDao nowDao;
 	
 	@GetMapping("/")
+	//@ResponseBody
 	public String index() {
 		return "資料庫現在時刻:" + nowDao.now(); 
 	}
