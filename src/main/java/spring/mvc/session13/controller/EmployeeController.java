f3637fd786ad5987bfdcf2e9802edc9d870cea4a
teacher <teacher@BE-403->
2022-08-15 19:35:02 +0800

    s

--- src/main/java/spring/mvc/session13/controller/EmployeeController.java
+++ src/main/java/spring/mvc/session13/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -52,6 +53,17 @@ public class EmployeeController {
 		return "redirect:./";
 	}
 	
+	@PutMapping("/")
+	public String update(@Valid Employee employee, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "PUT");
+			model.addAttribute("employees", employeeDao.query());
+			model.addAttribute("employee", employee);
+			return "session13/employee";
+		}
+		employeeDao.update(employee);
+		return "redirect:./";
+	}
 	
 	
 }
