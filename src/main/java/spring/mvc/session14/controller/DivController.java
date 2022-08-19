20db9770e3e4e1314f25c2ffe0e6ca687c46a61e
teacher <teacher@BE-403->
2022-08-19 19:26:07 +0800

    web

--- src/main/java/spring/mvc/session14/controller/DivController.java
+++ src/main/java/spring/mvc/session14/controller/DivController.java
@@ -1,11 +1,16 @@
 package spring.mvc.session14.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.mvc.session14.entity.Div;
 
@@ -24,5 +29,13 @@ public class DivController {
 		div.setResult(result);
 		model.addAttribute("result", div.getResult());
 		return "session14/div";
+	}
+	
+	// 捕獲使用者輸入格式不正確的例外, 數學錯誤的例外
+	@ExceptionHandler({BindException.class, ArithmeticException.class})
+	@ResponseBody
+	public String catchException(Exception ex, Model mode, HttpServletRequest request) {
+		String referer = request.getHeader("Referer");
+		return ex + ", " + referer;
 	}
 }
