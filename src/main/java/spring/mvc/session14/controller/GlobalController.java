aa984b7d7851d816a420cb605731789d9b7998ef
teacher <teacher@BE-403->
2022-08-19 19:58:59 +0800

    web

--- src/main/java/spring/mvc/session14/controller/GlobalController.java
+++ src/main/java/spring/mvc/session14/controller/GlobalController.java
@@ -3,7 +3,6 @@ package spring.mvc.session14.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
