96c35ad9dc9fb1cf6b019077d225ec79f4defe8b
teacher <teacher@BE-403->
2022-08-22 21:29:39 +0800

    w

--- src/main/java/spring/mvc/session15/repository/UserRepository.java
+++ src/main/java/spring/mvc/session15/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package spring.mvc.session15.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import antlr.collections.List;
 import spring.mvc.session15.entity.User;
 
 /*
@@ -15,4 +17,17 @@ import spring.mvc.session15.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 	// 根據 name 來取得 User
 	User getByName(String name);
+	
+	// Where name LIKE ? AND id >= ?
+	List<User> getByNameStartingWithAndIdGreaterThanEqual(String name, Long id);
+	
+	// Where id in (?, ?, ?)
+	List<User> getByIdIn(List<Long> ids);
+	
+	// Where birth < ?
+	List<User> getByBirthLessThan(Date birth);
+	
+	// Where birth >= ? AND birth <= ?
+	// Where birth between ?(含) AND ?(含)
+	List<User> getByBirthBetween(Date birthBegin, Date birthEnd);
 }
